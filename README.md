e8e2365a2ba1a994a5341cb9cd8f72c520552763
philogicae <38438271+philogicae@users.noreply.github.com>
2023-09-26 10:38:39 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# IPFS-CID-in-Bytes32
+
+Convenient minimal library to convert IPFS CIDs from/to Bytes32 using pure Solidity
+
+## Installation
+
+```bash
+npm install ipfs-cid-in-bytes32
+```
+
+## Use
+
+```solidity
+import "ipfs-cid-in-bytes32/contracts/Bytes32IpfsCid.sol";
+```
+
+## License
+
+This project is released under the MIT License.
---
+++ contracts/Bytes32IpfsCid.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+import "base58-solidity/contracts/Base58.sol";
+
+/**
+ * @title bytes32IpfsCid
+ * @author @philogicae
+ * @notice Convenient minimal library to convert IPFS CIDs from/to bytes32 using pure Solidity.
+ * Storing IPFS CIDs/hashs/URIs can be gas expensive https://ethereum.stackexchange.com/a/61104
+ * The idea here is to precompute offchain the bytes32 version of our IPFS CID, by calling a view function, before to store it.
+ * We can then recover the CID in the same manner later https://ethereum.stackexchange.com/a/17112
+ * Meant to be used ONLY in view/pure functions.
+ */
+library Bytes32IpfsCid {
+    function encode(string memory cid) public pure returns (bytes32 shorten) {
+        bytes memory result = Base58.decode(bytes(cid));
+        assembly {
+            shorten := mload(add(result, 34))
+        }
+    }
+
+    function decode(bytes32 data) public pure returns (string memory) {
+        return string(Base58.encode(abi.encodePacked(bytes2(0x1220), data)));
+    }
+}
